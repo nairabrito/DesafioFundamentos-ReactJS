3a6eb4b4e156c71aead4432cb34be607a4790900
Naira Brito <naira.sistemas@gmail.com>
2020-11-02 20:51:41 -0300

    Finalizado desafio-fundamentos-reactjs

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -15,12 +15,12 @@ const Header: React.FC<HeaderProps> = ({ size = 'large' }: HeaderProps) => (
     <header>
       <img src={Logo} alt="GoFinances" />
       <nav>
-        {
-          // Todo
-        }
+       <Link to="/">Listagem</Link>
+       <Link to="/import">Importar</Link>
       </nav>
     </header>
   </Container>
 );
 
 export default Header;
+
